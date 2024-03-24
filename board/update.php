772d90a145288fb44fa30a52e75be64c5efa6d39
ingeon123 <117888884+ingeon123@users.noreply.github.com>
2024-03-24 15:22:04 +0900

    Update update.php

--- board/update.php
+++ board/update.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="css/board.css">
 
 <?php
-    $db = new mysqli("localhost","root","1755","board");
+    $db = new mysqli("localhost","root","1755","web");
     
     // 수정
     if($_POST['act']=='u' && $_POST['no']){
@@ -32,26 +32,13 @@
         }
     }
     
-    //댓글 작성
-    // elseif($_POST['act']=='co' && $_POST['no']){
-    //     $query = "insert coment 
-    //                     set name = '" . $_POST['name'] . "', 
-    //                         content = '{$_POST['content']}'";
-    //     $res = $db->query($query);
-    //     if($res){
-    //         echo "<script>alert('댓글 작성 성공');location.href='view.php?no".$_POST['no']."'</script>;";
-    //     }
-    //     else {
-    //         echo "<script>alert('댯굴 작성 실패';)location.href='form.php?no".$_POST['no']."'</script>;";
-    //     }
-    // }
-
     //글작성
     else{
-        $query = "insert board 
-                        set name = '" . $_POST['name'] . "', 
-                            title = '{$_POST['title']}', 
-                            content = '{$_POST['content']}'";
+        $query = $query = "INSERT INTO board (name, title, content) 
+                            VALUES ('" . $_POST['name'] . "', 
+                            '" . $_POST['title'] . "', 
+                            '" . $_POST['content'] . "')";
+
         $res = $db->query($query);
 
         if($res){
@@ -59,6 +46,7 @@
         }
         else {
             echo "<script>alert('글작성 실패');location.href='form.php'</script>;";
+            //echo "Query Failed: (" . $db->errno . ") " . $db->error;
         }
     }
 ?>
